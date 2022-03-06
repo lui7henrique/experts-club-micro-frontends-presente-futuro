f4e0f2b1faed0213dc3cf4d91f69b39083db76ba
Luiz Henrique <7henrique18@gmail.com>
2022-03-06 16:56:31 -0300

    :tada: loadComponents function

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import Button from "dsl/Button";
+import { System } from "./utils/loadComponents";
 
 import "./index.scss";
 
 const App = () => {
+  const [system, setSystem] = useState(undefined);
+
+  useEffect(() => {
+    console.log(system);
+  }, [system]);
+
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      <div>Name: host</div>
-      <div>Framework: react</div>
-      <div>Language: TypeScript</div>
-      <div>CSS: Tailwind</div>
-      <Button />
+      <button
+        onClick={() => {
+          setSystem({
+            url: "http://localhost:8081/remoteEntry.js",
+            scope: "dsl",
+            module: "./Button",
+          });
+        }}
+      >
+        Carregar módulo
+      </button>
+      <System system={system}></System>
     </div>
   );
 };
